68bf50d9779f2e2755d1b2a9f61c06efb6d78940
glazernir <glazer.nir@campus.technion.ac.il>
2023-06-27 08:18:14 +0000

    #1

---
+++ malloc_2.cpp
@@ -0,0 +1,115 @@
+//
+// Created by student on 6/26/23.
+//
+#include <unistd.h>
+#include <stdio.h>
+#include <iostream>
+#include <cstring>
+
+struct MallocMetaDatta{
+    size_t size;
+    bool is_free;
+    MallocMetaDatta* next;
+    MallocMetaDatta* prev;
+};
+
+MallocMetaDatta* metaData_first = nullptr;
+MallocMetaDatta* mettaData_last = nullptr;
+
+void* smalloc(size_t size){
+    if(size < 0 || size > 100000000){
+        return nullptr;
+    }
+    MallocMetaDatta* tmp = metaData_first;
+    while(tmp != nullptr){
+        if(tmp->size >= size && tmp->is_free == true){
+            return tmp;
+        }
+        tmp = tmp->next;
+    }
+    void* checkAllocate = sbrk(sizeof(MallocMetaDatta) + size);
+    if(checkAllocate == (void*)-1){
+        return NULL;
+    }
+    auto* newAllocate = static_cast<MallocMetaDatta*>(checkAllocate);
+    newAllocate->size = size;
+    newAllocate->is_free = false;
+    newAllocate->next = nullptr;
+    if(metaData_first == nullptr){
+        newAllocate->prev = nullptr;
+        metaData_first = newAllocate;
+        mettaData_last = newAllocate;
+    }
+    else{
+        mettaData_last->next = newAllocate;
+        newAllocate->prev = mettaData_last;
+        mettaData_last = mettaData_last->next;
+    }
+    return (void*)((long)newAllocate+sizeof(MallocMetaDatta));
+}
+
+void* scalloc(size_t num, size_t size){
+    if(size == 0 || num == 0){
+        return nullptr;
+    }
+    void* isBlockExist = smalloc(num*size);
+    if(isBlockExist != nullptr){
+        memset(isBlockExist,0,num*size);
+        return isBlockExist;
+    }
+    return nullptr;
+}
+
+void sfree(void* p){
+    if(p == nullptr){
+        return;
+    }
+    MallocMetaDatta* tmp = metaData_first;
+    while(tmp != nullptr){
+        if((void*)(long(tmp) + sizeof(MallocMetaDatta)) == p){
+            if(tmp->is_free == true){
+                return;
+            }
+            tmp->is_free = true;
+            return;
+        }
+        tmp = tmp->next;
+    }
+}
+void* srealloc(void* oldp, size_t size){
+    if(size == 0 || size > 100000000){
+        return nullptr;
+    }
+    if(oldp == nullptr){
+        void* newAllocate = smalloc(size);
+        return newAllocate;
+    }
+    MallocMetaDatta* tmp = metaData_first;
+    size_t oldpSize;
+    while(tmp != nullptr){
+        if((void*)(long(tmp) + sizeof(MallocMetaDatta)) == oldp){
+            oldpSize = tmp->size;
+            if(tmp->size >= size){
+                return oldp;
+            }
+        }
+        tmp = tmp->next;
+    }
+    void* newAllocation = smalloc(size);
+    if(newAllocation == nullptr){
+        return nullptr;
+    }
+    memcpy(newAllocation,oldp,oldpSize);
+    sfree(oldp);
+    return newAllocation;
+}
+
+
+
+
+
+
+
+
+
+
